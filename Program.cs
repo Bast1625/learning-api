0254fbd6c4dbbf6081f28b7526e88a188e244467
Bast <sebastienbaillon1625@gmail.com>
2024-06-25 13:08:48 -0400

    CORS

--- Program.cs
+++ Program.cs
@@ -6,6 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(cors =>
+{
+    cors.AddPolicy("AllowOrigin", options =>
+    {
+        options.WithOrigins("https://happy-glacier-0102e450f.5.azurestaticapps.net")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -16,6 +25,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowOrigin");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
